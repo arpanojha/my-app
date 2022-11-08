14d00083d0f0c3bab71ddd609df068964fc9bd86
Parth Kapil <parthkapil@Parths-MacBook-Pro.local>
2022-11-08 02:55:15 -0500

    made minor fixes

--- src/component/Hero/Home.js
+++ src/component/Hero/Home.js
@@ -1,53 +1,105 @@
-import React from "react"
-import "./Home.css"
-import hero from "../pic/hero1.png"
-import skill1 from "../pic/skill1.png"
-import skill2 from "../pic/skill2.png"
-import skill3 from "../pic/skill3.png"
-import find1 from "../pic/find1.png"
-import gh from "../pic/gh.png"
-import twtr from "../pic/twitter.png"
-import { Typewriter } from "react-simple-typewriter"
+import React from "react";
+import "./Home.css";
+import hero from "../pic/hero1.png";
+import skill1 from "../pic/skill1.png";
+import skill2 from "../pic/skill2.png";
+import skill3 from "../pic/skill3.png";
+import find1 from "../pic/find1.png";
+import gh from "../pic/gh.png";
+import twtr from "../pic/twitter.png";
+import { Typewriter } from "react-simple-typewriter";
+import {
+	FaDribbbleSquare,
+	FaFacebookSquare,
+	FaGithubSquare,
+	FaInstagram,
+	FaTwitterSquare,
+	FaLinkedinIn,
+} from "react-icons/fa";
 
 const Home = () => {
-  return (
-    <>
-      <section className='hero' id='home'>
-        <div className='container f_flex top'>
-          <div className='left top'>
-            <h3>WELCOME</h3>
-            <h1>
-              Hi, I’m <span>Arpan Ojha</span>
-            </h1>
-            <h2>
-              a
-              <span>
-                <Typewriter words={[" Graduate Student."," Data analyst."," Professional Coder.", " Developer."]} loop cursor cursorStyle='|' typeSpeed={70} deleteSpeed={50} delaySpeed={1000} />
-              </span>
-            </h2>
+	return (
+		<>
+			<section className="hero" id="home">
+				<div className="container f_flex top">
+					<div className="left top">
+						<h3>WELCOME</h3>
+						<h1>
+							Hi, I’m <span>Arpan Ojha</span>
+						</h1>
+						<h2>
+							a
+							<span>
+								<Typewriter
+									words={[
+										" Graduate Student.",
+										" Data analyst.",
+										" Professional Coder.",
+										" Developer.",
+									]}
+									loop
+									cursor
+									cursorStyle="|"
+									typeSpeed={70}
+									deleteSpeed={50}
+									delaySpeed={1000}
+								/>
+							</span>
+						</h2>
 
-            <p></p>
-          
-            <div className='hero_btn d_flex'>
-              <div className='col_1'>
-                <h4>I am looking for full-time positions in Data science, Analytics and software engineering</h4>
-                <h4>FIND ME</h4>
-                <div className='button'>
-                  <button className='btn_shadow' onClick="window.location.href='https://twitter.com/ojhaarpan18';">
-                    {/* <i class='fab fa-facebook-f'></i> */}
-                    <img src={twtr} alt='' />
-                  </button> 
-                  <button className='btn_shadow' onClick="window.location.href='https://www.linkedin.com/in/arpan-96/';">
-                    <i class='fab fa-linkedin-in'></i>
-                    <img src={find1} alt='' />
-                  </button>
-                  <button className='btn_shadow' onClick="window.location.href='https://github.com/arpanojha';">
-                    <i class='fab fa-linkedin-in'></i>
-                    <img src={gh} alt='' />
-                  </button>
-                </div>
-              </div>
-              {/* <div className='col_1'>
+						<p></p>
+
+						<div className="hero_btn d_flex">
+							<div className="col_1">
+								<h4>
+									I am looking for full-time positions in Data science, Analytics and software
+									engineering
+								</h4>
+								<h4>FIND ME</h4>
+								<div className="button">
+									<a href="https://www.google.com/" target="_blank">
+										<button className="btn_shadow">
+											<FaTwitterSquare size={30} />
+										</button>
+									</a>
+
+									<a href="https://www.google.com/" target="_blank">
+										<button className="btn_shadow">
+											<FaLinkedinIn size={30} />
+										</button>
+									</a>
+
+									<a href="https://www.google.com/" target="_blank">
+										<button className="btn_shadow">
+											<FaGithubSquare size={30} />
+										</button>
+									</a>
+
+									{/* 
+                  <button
+										className="btn_shadow"
+										onClick="window.location.href='https://twitter.com/ojhaarpan18';"
+									>
+										<img src={twtr} alt="" />
+									</button>
+									<button
+										className="btn_shadow"
+										onClick="window.location.href='https://www.linkedin.com/in/arpan-96/';"
+									>
+										<i class="fab fa-linkedin-in"></i>
+										<img src={find1} alt="" />
+									</button>
+									<button
+										className="btn_shadow"
+										onClick="window.location.href='https://github.com/arpanojha';"
+									>
+										<i class="fab fa-linkedin-in"></i>
+										<img src={gh} alt="" />
+									</button>
+                  */}
+								</div>
+							</div>
+							{/* <div className='col_1'>
                 <h4>BEST SKILL ON</h4>
                 <button className='btn_shadow'>
                   <img src={skill1} alt='' />
@@ -59,17 +111,17 @@ const Home = () => {
                   <img src={skill3} alt='' />
                 </button>
               </div> */}
-            </div>
-          </div>
-          <div className='right'>
-            <div className='right_img'>
-              <img src={hero} alt='' />
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  )
-}
+						</div>
+					</div>
+					<div className="right">
+						<div className="right_img">
+							<img src={hero} alt="" />
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
+};
 
-export default Home
+export default Home;
--- src/component/Resume/ResumeApi.js
+++ src/component/Resume/ResumeApi.js
@@ -1,54 +1,53 @@
 const ResumeApi = [
-  {
-    id: 1,
-    category: "education",
-    year: "Indiana University (2021 - 2023)",
-    title: "MS. Data Science",
-    desc: "Primary courses: Advanced Database Concepts, Computer vision, Elements of Artificial Intelligence, Applied machine learning, Human Robot interaction.",
-    rate: "3.8/4",
-  },
-  {
-    id: 2,
-    category: "education",
-    year: "Indian Institute of Technology, Guwahati (2014 - 2018) ",
-    title: "BTech. Engineering Physics with minor in ECE",
-    desc: "Solid fundamentals in Electromagnetics, Microprocessor architecture , Computational Physics, Quantum Physics, Statistical Physics and Signal Processing.",
-    rate: "",
-  },
-  
-  {
-    id: 3,
-    category: "experience",
-    year: "A310,C322,C290 (2022 - present) ",
-    title: "Graduate teaching assistant",
-    desc: "Graduate teaching assistant in \n CSCI C322: Object Oriented Software design(Fall and Spring), CSCI C290: Programming Quantum Computers, CSCI A310: Problem solving with Data. Primary skills developed: Java, Python, Q# and Amazon Bracket",
-    rate: " ",
-  },
-  {
-    id: 4,
-    category: "experience",
-    year: "FADS fellowship (2022 - 2022) ",
-    title: "Summer research fellow",
-    desc: "FADS fellowship to study data science approaches to qualitative coding on a 30M interview dataset under the guidance of Dr. Emily Meanwell,ocial Science Research Commons, Indiana University.",
-    rate: " ",
-  },
-  {
-    id: 5,
-    category: "experience",
-    year: "Grad student club of Quantum Information Sciences (2022 - present) ",
-    title: "President",
-    desc: "Assist in grant writing, develop state of the art techniques for quantum devices, co-ordinate with faculty clubs and QED-C speakers.",
-    rate: "",
-  },
-  {
-    id: 6,
-    category: "experience",
-    year: "Wipro/Cisco (2018 - 2021) ",
-    title: "Software Engineer",
-    desc: "Cyber security analyst and research on Cisco's flagship security appliance products, Lead threat modelling for two releases, Developed advanced security features. Experienced in WSA, ESA , SMA , ISE , WCCPv2, Cognitive threat analysis, MUS/VPN, CSR and ASA routers.",
-    rate: "",
-  },
-  
-]
+	{
+		id: 1,
+		category: "education",
+		year: "Indiana University (2021 - 2023)",
+		title: "MS. Data Science",
+		desc: "Primary courses: Advanced Database Concepts, Computer vision, Elements of Artificial Intelligence, Applied machine learning, Human Robot interaction.",
+		rate: "3.8/4",
+	},
+	{
+		id: 2,
+		category: "education",
+		year: "Indian Institute of Technology, Guwahati (2014 - 2018) ",
+		title: "BTech. Engineering Physics with minor in ECE",
+		desc: "Solid fundamentals in Electromagnetics, Microprocessor architecture , Computational Physics, Quantum Physics, Statistical Physics and Signal Processing.",
+		rate: "",
+	},
 
-export default ResumeApi
+	{
+		id: 3,
+		category: "experience",
+		year: "A310,C322,C290 (2022 - present) ",
+		title: "Graduate teaching assistant",
+		desc: "Graduate teaching assistant in \n CSCI C322: Object Oriented Software design(Fall and Spring), CSCI C290: Programming Quantum Computers, CSCI A310: Problem solving with Data. Primary skills developed: Java, Python, Q# and Amazon Bracket",
+		rate: "",
+	},
+	{
+		id: 4,
+		category: "experience",
+		year: "FADS fellowship (2022 - 2022) ",
+		title: "Summer research fellow",
+		desc: "FADS fellowship to study data science approaches to qualitative coding on a 30M interview dataset under the guidance of Dr. Emily Meanwell,ocial Science Research Commons, Indiana University.",
+		rate: "",
+	},
+	{
+		id: 5,
+		category: "experience",
+		year: "Grad student club of Quantum Information Sciences (2022 - present) ",
+		title: "President",
+		desc: "Assist in grant writing, develop state of the art techniques for quantum devices, co-ordinate with faculty clubs and QED-C speakers.",
+		rate: "",
+	},
+	{
+		id: 6,
+		category: "experience",
+		year: "Wipro/Cisco (2018 - 2021) ",
+		title: "Software Engineer",
+		desc: "Cyber security analyst and research on Cisco's flagship security appliance products, Lead threat modelling for two releases, Developed advanced security features. Experienced in WSA, ESA , SMA , ISE , WCCPv2, Cognitive threat analysis, MUS/VPN, CSR and ASA routers.",
+		rate: "",
+	},
+];
+
+export default ResumeApi;
